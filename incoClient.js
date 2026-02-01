6807f5d13f5e134444d125ea013e4edd4e585516
Domain Genius <77184176+Miracle395@users.noreply.github.com>
2026-02-01 06:38:29 +0100

    Update incoClient.js

--- incoClient.js
+++ incoClient.js
@@ -1,21 +1,25 @@
 // incoClient.js
-const pkg = require("@inco/solana-sdk"); // CommonJS
-const { Client, encryption } = pkg;
-
+const { createClient, encryption } = require("@inco/solana-sdk");
 const { Connection, clusterApiUrl } = require("@solana/web3.js");
 
 const network = process.env.SOLANA_NETWORK || "devnet";
-const programId = process.env.INCO_PROGRAM_ID || "YourProgramIdHere";
+const programId = process.env.INCO_PROGRAM_ID;
 
 function createIncoClient({ wallet }) {
-  const connection = new Connection(clusterApiUrl(network), "confirmed");
+  const connection = new Connection(
+    clusterApiUrl(network),
+    "confirmed"
+  );
 
-  return new Client({
+  return createClient({
     network,
     wallet,
     connection,
-    programId,
+    programId
   });
 }
 
-module.exports = { createIncoClient, encryption };
+module.exports = {
+  createIncoClient,
+  encryption
+};
